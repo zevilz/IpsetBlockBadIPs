fbc66f65634aca19aa7817d53118614af82e6d37
zEvilz <zevilz13@yandex.ru>
2017-12-24 10:31:56 +0500

    updated

--- ipset_update.sh
+++ ipset_update.sh
@@ -3,7 +3,7 @@
 # URL: https://github.com/zevilz/IpsetBlockBadIPs
 # Author: zEvilz
 # License: MIT
-# Version: 1.0.3
+# Version: 1.0.4
 
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
