a0a8e7da524b96c71b148c60d685a661308a9dde
zEvilz <zevilz13@yandex.ru>
2018-11-26 14:49:03 +0500

    Added whitelist support

--- ipset_update.sh
+++ ipset_update.sh
@@ -3,7 +3,7 @@
 # URL: https://github.com/zevilz/IpsetBlockBadIPs
 # Author: zEvilz
 # License: MIT
-# Version: 1.0.4
+# Version: 1.1.0
 
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
@@ -20,24 +20,48 @@ then
 	exit 1
 fi
 CUR_PATH=$(dirname $0)
+
 echo -n "Download blacklist from stopforumspam.com..."
 cd $CUR_PATH
 wget -qN http://www.stopforumspam.com/downloads/listed_ip_$1.zip
-unzip -oq listed_ip_$1.zip
-echo "Done"
-echo -n "Applying blacklist to IPSET..."
-ipset -q -N blacklist iphash
-ipset -q -F blacklist
-xfile=$(cat $CUR_PATH/listed_ip_$1.txt)
-for ipaddr in $xfile
-do
-	ipset -exist -A blacklist $ipaddr
-done
-echo "Done"
-echo -n "Applying blacklist to IPTABLES..."
-iptables -I INPUT -m set --match-set blacklist src -j REJECT
-iptables -I INPUT -m set --match-set blacklist src -j LOG --log-prefix "REJECT blacklist entry"
-echo "Done"
-echo -n "Num of blacklisted IPs..."
-sleep 5
-ipset -L blacklist | grep -A999999999 'Members:' | tail -n +2 | wc -l
+
+if ! [ -f listed_ip_$1.zip ]; then
+
+	echo "Can't download!"
+	exit 1
+
+else
+
+	unzip -oq listed_ip_$1.zip
+	echo "Done"
+
+	echo -n "Applying blacklist to IPSET..."
+	ipset -q -N blacklist iphash
+	ipset -q -F blacklist
+	xfile=$(cat $CUR_PATH/listed_ip_$1.txt)
+	for ipaddr in $xfile
+	do
+		ipset -exist -A blacklist $ipaddr
+	done
+	echo "Done"
+
+	if [ -f whitelist ]; then
+		echo -n "Remove whitelisted IPs from blacklist..."
+		wxfile=$(cat $CUR_PATH/whitelist)
+		for ipaddr in $wxfile
+		do
+			ipset del blacklist $ipaddr > /dev/null 2>/dev/null
+		done
+		echo "Done"
+	fi
+
+	echo -n "Applying blacklist to IPTABLES..."
+	iptables -I INPUT -m set --match-set blacklist src -j REJECT
+	iptables -I INPUT -m set --match-set blacklist src -j LOG --log-prefix "REJECT blacklist entry"
+	echo "Done"
+
+	echo -n "Num of blacklisted IPs..."
+	sleep 5
+	ipset -L blacklist | grep -A999999999 'Members:' | tail -n +2 | wc -l
+
+fi
